e9320b534e9de25dce59ad7951215409667d0b28
Michael Gicking <mgicking@bmi.com>
2025-10-14 09:34:09 -0500

    initial commit

---
+++ startup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo dnf install python3.12 -y
+sudo alternatives --install /usr/bin/python3 python3 /usr/bin/python3.12 1
+sudo alternatives --config python3
+
+sudo dnf install pkgconfig python3.12-devel gcc mariadb-devel python3.12-pip git mariadb.x86_64 -y
+
+sudo dnf module enable nodejs:20 -y
+sudo dnf install nodejs --setopt=install_weak_deps=False -y
+sudo npm install -g yarn
+
+wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox-0.12.6-1.centos8.x86_64.rpm
+sudo dnf install wkhtmltox-0.12.6-1.centos8.x86_64.rpm -y
+
+pip3 install frappe-bench
+
+cd ~
+bench init erpnext
