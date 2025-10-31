118aec748a908e8a444bcfd5ebfe688d6b7ee3df
Michael Gicking <mgicking@bmi.com>
2025-10-31 14:25:52 -0500

    Deprecate startup script

--- startup.sh
+++ startup.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-sudo dnf install python3.12 -y
-sudo alternatives --install /usr/bin/python3 python3 /usr/bin/python3.12 1
-sudo alternatives --config python3
+echo "This script is deprecated. Pleae check back later for an updated version."
+exit 1
 
-sudo dnf install pkgconfig python3.12-devel gcc mariadb-devel python3.12-pip git mariadb.x86_64 -y
+# sudo dnf install python3.12 -y
+# sudo alternatives --install /usr/bin/python3 python3 /usr/bin/python3.12 1
+# sudo alternatives --config python3
 
-sudo dnf module enable nodejs:20 -y
-sudo dnf install nodejs --setopt=install_weak_deps=False -y
-sudo npm install -g yarn
+# sudo dnf install pkgconfig python3.12-devel gcc mariadb-devel python3.12-pip git mariadb.x86_64 -y
 
-wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox-0.12.6-1.centos8.x86_64.rpm
-sudo dnf install wkhtmltox-0.12.6-1.centos8.x86_64.rpm -y
+# sudo dnf module enable nodejs:20 -y
+# sudo dnf install nodejs --setopt=install_weak_deps=False -y
+# sudo npm install -g yarn
 
-pip3 install frappe-bench
+# wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox-0.12.6-1.centos8.x86_64.rpm
+# sudo dnf install wkhtmltox-0.12.6-1.centos8.x86_64.rpm -y
 
-cd ~
-bench init erpnext
+# pip3 install frappe-bench
+
+# cd ~
+# bench init erpnext
